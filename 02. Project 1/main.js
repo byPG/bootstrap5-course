9d9024161e989c9c24267c7de5d286703153a83c
byPG <67753466+byPG@users.noreply.github.com>
2022-11-05 19:29:53 +0100

    Finish project 01 with course

---
+++ 02. Project 1/main.js
@@ -0,0 +1,9 @@
+
+//close nav
+const nav = document.querySelector('.navbar-collapse')
+
+document.addEventListener('click', () => {
+  if (nav.classList.contains('show')) {
+    nav.classList.remove('show')
+  }
+})
